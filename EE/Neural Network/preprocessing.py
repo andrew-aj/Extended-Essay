99fae03813c7eb1e0c48a8f32b3f190ae361fc40
cheezy1231 <andrew.knee.j@gmail.com>
2019-01-14 23:14:10 -0500

    Updated preprocessing

--- EE/Neural Network/preprocessing.py
+++ EE/Neural Network/preprocessing.py
@@ -3,20 +3,20 @@ from PIL import Image
 
 x = Image.open('main.jpg','r').resize((32,32))
 x = x.convert('L') 
-y = np.asarray(x.getdata(), dtype=np.float64).reshape((x.size[1],x.size[0]))
+y = np.asarray(x.getdata(), dtype=np.float64)#.reshape((x.size[1],x.size[0]))
 #to have just a list of pixed, comment out .reshape
 
 y = np.asarray(y,dtype=np.uint8)
 with np.nditer(y, op_flags=['readwrite']) as it:
 	for x in it:
 		if(x[...] > 128):
-			x[...] = 255
+			x[...] = 1
 			#in reality will be set to 1 to take up less memory
 			#only used 255 here so i could visualize it
 		else:
 			x[...] = 0
 
 #to stop conversion to image comment out the next two lines
-w = Image.fromarray(y,mode='L')
-w.save('out.jpg')
+#w = Image.fromarray(y,mode='L')
+#w.save('out.jpg')
 np.savetxt("foo.csv", y,fmt='%i')#, delimiter=",")
